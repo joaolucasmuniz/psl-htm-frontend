a12bee5c3fafca2d4a6cf0e8fe0dbebff66d2043
joaolucasmuniz <joaolucasmuniz12345@gmail.com>
2023-11-28 22:34:39 -0300

    Update page titles and add client details route

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,18 @@ import './App.css';
 import Login from './pages/login/login';
 import ListClients from './pages/clients/clients';
 import Layout from './componentes/layout';
+import ClientDetails from './pages/clientDetails/clientDetails';
 
 function App() {
   return (
 
     <Routes>
+      <Route path="/login" element={ <Login /> } />
+      <Route path="/clients/:id" element={ <ClientDetails /> } />
       <Route element={ <Layout /> }>
-        <Route path="/login" element={ <Login /> } />
         <Route path="/clients" element={ <ListClients /> } />
       </Route>
     </Routes>
-
   );
 }
 
