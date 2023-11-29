b8584e06f835c9e4dd77501df2519f2dc154045a
joaolucasmuniz <joaolucasmuniz12345@gmail.com>
2023-11-28 22:34:29 -0300

    Add TClientDetails type to types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,13 @@ export type CardProps = {
   id: number,
   name: string,
 };
+
+export type TClientDetails = {
+  id: number,
+  access_level: number,
+  address: string,
+  email: string,
+  name: string,
+  phone: string,
+  postalcode: string,
+};
