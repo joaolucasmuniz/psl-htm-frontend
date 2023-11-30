447127c0e68ffd2e5629d9c89775d010c117f21c
joaolucasmuniz <joaolucasmuniz12345@gmail.com>
2023-11-29 22:49:56 -0300

    Add login page with form and styling

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,8 +1,12 @@
+/* eslint-disable react/jsx-max-depth */
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { urlBase } from '../../helpers/urlBase';
 import { ErrorAlert } from '../../utils/alert';
 import Loading from '../../componentes/loading ';
+import loginIcon from '../../images/login.jpg';
+
+import styles from './login.module.css';
 
 function Login() {
   const [username, setUsername] = useState('');
@@ -26,8 +30,8 @@ function Login() {
     const data = await response.json();
     setLoading(false);
 
-    if (data.message || data.error) {
-      ErrorAlert(data.message || data.error);
+    if (data.error) {
+      ErrorAlert(data.error);
     } else {
       setLocalStorage(data.token);
       navigate('/clients');
@@ -41,25 +45,32 @@ function Login() {
   if (loading) return <Loading />;
 
   return (
-    <div>
-      <h1>Login</h1>
-
-      <form
-        onSubmit={ handleLogin }
-      >
-        <label htmlFor="username">
-          Usuário
-          <input
-            id="username"
-            type="text"
-            value={ username }
-            onChange={ handleUsername }
-          />
-        </label>
-        <button type="submit">
-          Entrar
-        </button>
-      </form>
+    <div className={ styles.loginContainer }>
+      <section className={ styles.login }>
+        <img src={ loginIcon } alt="login" />
+        <section className={ styles.loginForm }>
+          <div className={ styles.loginFormContainer }>
+            <h1>Login</h1>
+            <form
+              onSubmit={ handleLogin }
+            >
+              <label htmlFor="username">
+                Username
+                <input
+                  id="username"
+                  type="text"
+                  value={ username }
+                  onChange={ handleUsername }
+                  placeholder="Mariana.Vasconcelos"
+                />
+              </label>
+              <button type="submit">
+                Entrar
+              </button>
+            </form>
+          </div>
+        </section>
+      </section>
     </div>
   );
 }
